d4222e85a1f57e42461507cf508592d8e950025d
VojtechGero <vojtech.gero@tul.cz>
2024-10-16 20:54:52 +0200

    renamed password to passwordHash

--- EFModels/Migrations/20241016185350_RenamePassword.Designer.cs
+++ EFModels/Migrations/20241016185350_RenamePassword.Designer.cs
@@ -3,6 +3,7 @@ using EFModels.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EFModels.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20241016185350_RenamePassword")]
+    partial class RenamePassword
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- EFModels/Models/User.cs
+++ EFModels/Models/User.cs
@@ -7,7 +7,7 @@ public class User
     [Key]
     public int Id { get; set; }
     public string UserName { get; set; }
-    public string Password { get; set; }
+    public string PasswordHash { get; set; }
     public override string ToString()
     {
         return UserName;
