3155c93ba30a5c6ee9f915edabf8b2cfe5b398d9
VojtechGero <vojtech.gero@tul.cz>
2024-11-17 15:35:35 +0100

    all that slop

--- EFModels/Models/User.cs
+++ EFModels/Models/User.cs
@@ -9,6 +9,13 @@ public class User
     public string Name { get; set; }
     public string UserName { get; set; }
     public string PasswordHash { get; set; }
+    public string? Address { get; set; }
+    public string? BillingAddress { get; set; }
+    public bool? ProcessData { get; set; }
+    public bool? IsMale { get; set; } //Genesis 1:27
+    public int? Age { get; set; }
+    public string? FavouriteGerners { get; set; }
+    public string? Refferal { get; set; }
 
     public ICollection<Comment> Comments { get; set; }
     public ICollection<Book> FavouriteBooks { get; set; } = new HashSet<Book>();
---
+++ WEA_BE/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using WEA_BE.DTO;
+
+namespace WEA_BE.Services;
+
+public interface IUserService
+{
+    UserDetailDto? GetUserDetail(string userName);
+    bool UpdateUser(string userName, string? address, string? billingAddress, bool? processData, bool? isMale, int? age, List<string> FavouriteGerners, string? refferal);
+}
